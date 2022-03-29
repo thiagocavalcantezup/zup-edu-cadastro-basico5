7a0609d0e6258e5a8238317e90e73b9c7cfe7009
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-29 15:23:16 -0300

    Add Pet creation method to Controller

--- src/main/java/br/com/zup/handora/cadastrobasico5/controllers/PetController.java
+++ src/main/java/br/com/zup/handora/cadastrobasico5/controllers/PetController.java
@@ -1,8 +1,18 @@
 package br.com.zup.handora.cadastrobasico5.controllers;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.zup.handora.cadastrobasico5.models.Pet;
+import br.com.zup.handora.cadastrobasico5.models.PetDTO;
 import br.com.zup.handora.cadastrobasico5.repositories.PetRepository;
 
 @RestController
@@ -15,6 +25,18 @@ public class PetController {
 
     public PetController(PetRepository petRepository) {
         this.petRepository = petRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestBody @Valid PetDTO petDTO,
+                                       UriComponentsBuilder uriComponentsBuilder) {
+        Pet pet = petRepository.save(petDTO.paraPet());
+
+        URI location = uriComponentsBuilder.path(BASE_URI + "/{id}")
+                                           .buildAndExpand(pet.getId())
+                                           .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
 }
