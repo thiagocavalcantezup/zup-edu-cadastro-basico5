5f9ecf0a2eb455aa07d8058ac06ec29f348ebd62
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-29 17:18:32 -0300

    Fix error response class

--- src/main/java/br/com/zup/handora/cadastrobasico5/exceptions/ErroPadronizado.java
+++ src/main/java/br/com/zup/handora/cadastrobasico5/exceptions/ErroPadronizado.java
@@ -3,13 +3,22 @@ package br.com.zup.handora.cadastrobasico5.exceptions;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import org.springframework.validation.FieldError;
 
 public class ErroPadronizado {
 
+    @JsonProperty("codigo_http")
     private Integer codigoHttp;
+
+    @JsonProperty("mensagem_http")
     private String mensagemHttp;
+
+    @JsonProperty("mensagem_geral")
     private String mensagemGeral;
+
+    @JsonProperty("mensagens")
     private List<String> mensagens;
 
     public ErroPadronizado(Integer codigoHttp, String mensagemHttp, String mensagemGeral) {
@@ -20,7 +29,13 @@ public class ErroPadronizado {
     }
 
     public void adicionarErro(FieldError fieldError) {
-        mensagens.add(fieldError.getField() + ": " + fieldError.getDefaultMessage());
+        String field = fieldError.getField();
+
+        if (field.equals("dataNascimento")) {
+            field = "data_nascimento";
+        }
+
+        mensagens.add(field + ": " + fieldError.getDefaultMessage());
     }
 
     public void adicionarErro(String erro) {
