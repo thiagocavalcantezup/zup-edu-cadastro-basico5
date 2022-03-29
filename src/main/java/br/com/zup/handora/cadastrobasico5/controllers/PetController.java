9192728b27e7640d3f3f3275af3184afd2da1fc1
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-29 15:15:55 -0300

    Add Repository dependency to Controller

--- src/main/java/br/com/zup/handora/cadastrobasico5/controllers/PetController.java
+++ src/main/java/br/com/zup/handora/cadastrobasico5/controllers/PetController.java
@@ -3,10 +3,18 @@ package br.com.zup.handora.cadastrobasico5.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.zup.handora.cadastrobasico5.repositories.PetRepository;
+
 @RestController
 @RequestMapping(PetController.BASE_URI)
 public class PetController {
 
     public final static String BASE_URI = "/pets";
+
+    private final PetRepository petRepository;
+
+    public PetController(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
 
 }
