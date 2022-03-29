073a4c9fda4213f7d6f06cc7bd0389cee8dfeb17
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-29 17:15:01 -0300

    Improve messages for JSON parsing errors

--- src/main/java/br/com/zup/handora/cadastrobasico5/exceptions/CustomExceptionHandler.java
+++ src/main/java/br/com/zup/handora/cadastrobasico5/exceptions/CustomExceptionHandler.java
@@ -1,20 +1,28 @@
 package br.com.zup.handora.cadastrobasico5.exceptions;
 
+import java.time.DateTimeException;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.HandlerMethod;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErroPadronizado> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+    @ResponseBody
+    ErroPadronizado handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
         HttpStatus badRequestStatus = HttpStatus.BAD_REQUEST;
         Integer codigoHttp = badRequestStatus.value();
         String mensagemHttp = badRequestStatus.getReasonPhrase();
@@ -29,11 +37,14 @@ public class CustomExceptionHandler {
         );
         fieldErrors.forEach(erroPadronizado::adicionarErro);
 
-        return ResponseEntity.badRequest().body(erroPadronizado);
+        return erroPadronizado;
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<ErroPadronizado> handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
+    @ResponseBody
+    ErroPadronizado handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+                                                 HandlerMethod handlerMethod) {
         HttpStatus badRequestStatus = HttpStatus.BAD_REQUEST;
         Integer codigoHttp = badRequestStatus.value();
         String mensagemHttp = badRequestStatus.getReasonPhrase();
@@ -43,9 +54,28 @@ public class CustomExceptionHandler {
         ErroPadronizado erroPadronizado = new ErroPadronizado(
             codigoHttp, mensagemHttp, mensagemGeral
         );
-        erroPadronizado.adicionarErro(ex.getMostSpecificCause().toString());
 
-        return ResponseEntity.badRequest().body(erroPadronizado);
+        Throwable causaMaisEspecifica = ex.getMostSpecificCause();
+
+        if (causaMaisEspecifica instanceof InvalidFormatException) {
+            if (causaMaisEspecifica.getMessage()
+                                   .startsWith(
+                                       "Cannot deserialize value of type "
+                                               + "`br.com.zup.handora.cadastrobasico5.models.TipoPet`"
+                                               + " from String"
+                                   )) {
+                erroPadronizado.adicionarErro(
+                    "O tipo fornecido possui um valor que não é aceito pela API. Valores aceitos: CAO, GATO."
+                );
+            }
+        } else if (causaMaisEspecifica instanceof DateTimeParseException
+                || causaMaisEspecifica instanceof DateTimeException) {
+            erroPadronizado.adicionarErro(
+                "A data fornecida está em um formato incorreto. O formato correto é: dd/MM/yyyy."
+            );
+        }
+
+        return erroPadronizado;
     }
 
 }
