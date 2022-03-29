ee834e4275cf5262730a4e39f8e3c6f7f2c60642
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-29 16:15:44 -0300

    Add config to ignore enum case in request body

--- src/main/java/br/com/zup/handora/cadastrobasico5/CadastroBasico5Application.java
+++ src/main/java/br/com/zup/handora/cadastrobasico5/CadastroBasico5Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CadastroBasico5Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CadastroBasico5Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CadastroBasico5Application.class, args);
+    }
 
 }
